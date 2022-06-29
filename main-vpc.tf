76b63a08a86835d26d8cfaeca21b44b3535a5a6c
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-06-29 19:04:39 +0000

    Updated region in main-vpc.tf file

--- main-vpc.tf
+++ main-vpc.tf
@@ -11,7 +11,10 @@ provider "aws" {
   # Configuration options
 }
 
-
+locals {
+  region = "eu-west-1"
+  #region = var.region
+}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -19,7 +22,7 @@ module "vpc" {
   name = "main-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
