6661e7ac279028bc5a9a22ec3a7afe162b82d88d
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-06-29 19:24:11 +0000

    Update IP address

--- main-vpc.tf
+++ main-vpc.tf
@@ -24,10 +24,11 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.16.0/24", "10.0.17.0/24", "10.0.18.0/24"]
 
   enable_nat_gateway = true
+  single_nat_gateway = false
   enable_vpn_gateway = false
 
   tags = {
