0ff56394ab37beec9d003ef40a997523d69d977c
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-01 14:44:05 +0000

    Updating IP CIDR range to 10.1/16 for spoke VPC 1

--- main-vpc.tf
+++ main-vpc.tf
@@ -21,13 +21,13 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "main-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "10.1.0.0/16"
 
   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.16.0/24", "10.0.17.0/24", "10.0.18.0/24"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  # public_subnets  = ["10.0.16.0/24", "10.0.17.0/24", "10.0.18.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   single_nat_gateway = false
   enable_vpn_gateway = false
 
