681c9ab5f27b7dba97a1b3ce3b20d353a7983446
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-03 11:47:46 +0000

    Fix ports

--- main-vpc.tf
+++ main-vpc.tf
@@ -92,11 +92,11 @@ resource "aws_security_group" "allow_testing_connectivity" {
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 443
+    from_port        = 0
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
