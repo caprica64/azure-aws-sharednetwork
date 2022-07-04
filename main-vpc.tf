929f9a677f9b034b5995eec3be57ea96b7204255
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-04 13:14:52 +0000

    subnet_id      = "rtb-0790923bc7463f563" ##module.vpc.private_subnets

--- main-vpc.tf
+++ main-vpc.tf
@@ -86,7 +86,7 @@ resource "aws_default_route_table" "main_private" {
 ## Associations
 #
 resource "aws_route_table_association" "main" {
-  subnet_id      = module.vpc.private_subnets
+  subnet_id      = "rtb-0790923bc7463f563" ##module.vpc.private_subnets
   route_table_id = aws_default_route_table.main_private
 }
 
