32909c2b9d571c787fe3ae0fdcda7c19aae44fcd
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-04 12:18:18 +0000

    Check default RT behaviour

--- main-vpc.tf
+++ main-vpc.tf
@@ -64,23 +64,23 @@ module "vpc" {
 #
 ## Outbound routes
 #
-# resource "aws_private_route_table" "example" {
-#   default_route_table_id = aws_vpc.example.default_route_table_id
-# 
-#   route {
-#     cidr_block = "10.0.1.0/24"
-#     gateway_id = aws_internet_gateway.example.id
-#   }
-# 
-#   route {
-#     ipv6_cidr_block        = "::/0"
-#     egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
-#   }
-# 
-#   tags = {
-#     Name = "example"
-#   }
-# }
+resource "aws_default_route_table" "example" {
+  default_route_table_id = aws_vpc.example.default_route_table_id
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = tgw-049f907ea0736b595
+  }
+
+  route {
+  #   ipv6_cidr_block        = "::/0"
+  #   egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  # }
+
+  tags = {
+    Name = "example"
+  }
+}
 
 ################################################################################
 # VPC Attachment section
