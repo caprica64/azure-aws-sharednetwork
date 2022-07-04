b03a4b9f45e5c7950471e8c0cb6507f2b37b3ff1
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-04 10:03:51 +0000

    Enable ICMP testes

--- main-vpc.tf
+++ main-vpc.tf
@@ -110,6 +110,17 @@ resource "aws_security_group_rule" "ssh_in" {
   description        = "Allow inbound SSH access the EC2 instances"
 }
 
+resource "aws_security_group_rule" "icmp_in" {
+  type               = "ingress"
+  from_port          = 0
+  to_port            = 0
+  protocol           = "icmp"
+  cidr_blocks        = ["0.0.0.0/0"]
+  security_group_id  = aws_security_group.allow_testing_connectivity.id
+  #name               = "ICMP inbound"
+  description        = "Allow inbound ICMP to the EC2 instances"
+}
+
 resource "aws_security_group_rule" "all_out" {
   type               = "egress"
   from_port          = 0
