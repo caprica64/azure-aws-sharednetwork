5fd5bf0fed1a0c8b0c93d67ff5f77ca1e04ad528
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-05 14:30:36 +0000

    Create new VPC without using modules

---
+++ main-vpc.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+	aws = {
+	  source = "hashicorp/aws"
+	  version = "4.20.1"
+	}
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "eu-west-1"
+}
+
+locals {
+  name   = "Spoke1"
+  region = "eu-west-1"
+  #region = var.region
+}
+
+################################################################################
+# VPC Module
+################################################################################
+
+#
+## Main VPC
+#
+resource "aws_vpc" "spoke1" {
+  cidr_block            = "10.1.0.0/16"
+  instance_tenancy      = "default"
+  
+  enable_dns_support    = true
+  enable_dns_hostnames  = true
+
+  tags = {
+    Name                = "Spoke1-VPC"
+    Project             = "Azure-AWS"
+  }
+}
+
+
+
+
+
+
