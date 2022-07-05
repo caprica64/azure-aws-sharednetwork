b6eabd03cebd503bc58cf66e94fefe3253de6f02
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-05 13:18:56 +0000

    Replaces private with infra subnets

--- main-vpc-module.tf
+++ main-vpc-module.tf
@@ -33,14 +33,14 @@ module "vpc" {
   cidr = "10.1.0.0/16"
 
   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  infra_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   # public_subnets  = ["10.0.16.0/24", "10.0.17.0/24", "10.0.18.0/24"]
 
   enable_nat_gateway = false
   single_nat_gateway = false
   enable_vpn_gateway = false
 
-  create_infra_subnet_route_table = false
+  #create_infra_subnet_route_table = false
 
   manage_default_network_acl = true
   default_network_acl_tags   = { Name = "${local.name}-default" }
