091a63edbf41520fac94e6dde1465f9b19ef805c
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-05 12:10:24 +0000

    Rollback

--- main-vpc.tf
+++ main-vpc.tf
@@ -94,8 +94,7 @@ resource "aws_default_route_table" "main_private" {
 ## Associations
 
 resource "aws_route_table_association" "main" {
-  #subnet_id      = module.vpc.aws_default_route_table.default[0]
-  subnet_id      = module.vpc.aws_route_table_association.private[0]
+  subnet_id      = module.vpc.aws_default_route_table.default[0]
   route_table_id = aws_default_route_table.main_private.id
 }
 
