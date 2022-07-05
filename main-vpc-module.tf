8897c205c92f9ca28b0ec4df345081a809f1d803
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-05 13:20:59 +0000

    intra_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]

--- main-vpc-module.tf
+++ main-vpc-module.tf
@@ -33,7 +33,7 @@ module "vpc" {
   cidr = "10.1.0.0/16"
 
   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  infra_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  intra_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   # public_subnets  = ["10.0.16.0/24", "10.0.17.0/24", "10.0.18.0/24"]
 
   enable_nat_gateway = false
