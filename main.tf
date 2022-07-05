1ff6312ffb752278445a6dadbd69080e84e68b19
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-05 19:55:47 +0000

    Create private and public subnets

--- main.tf
+++ main.tf
@@ -41,3 +41,50 @@ resource "aws_vpc" "spoke1" {
 	Project             = "Azure-AWS"
   }
 }
+
+#
+## Subnets
+#
+resource "aws_subnet" "PublicSubnet1a" {
+  cidr_block = "10.1.0.0/24"
+  map_public_ip_on_launch = false
+  vpc_id = aws_vpc.VPC.id
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+	Name = "Public Subnet AZ 1a"
+  }
+}
+
+resource "aws_subnet" "PublicSubnet1c" {
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = false
+  vpc_id = aws_vpc.VPC.id
+  availability_zone = data.aws_availability_zones.available.names[2]
+
+  tags = {
+	Name = "Public Subnet AZ 1c"
+  }
+}
+
+resource "aws_subnet" "PrivateSubnet1a" {
+  cidr_block = "10.1.10.0/24"
+  map_public_ip_on_launch = false
+  vpc_id = aws_vpc.VPC.id
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+	Name = "Private Subnet AZ 1a"
+  }
+}
+
+resource "aws_subnet" "PrivateSubnet1c" {
+  cidr_block = "10.1.11.0/24"
+  map_public_ip_on_launch = false
+  vpc_id = aws_vpc.VPC.id
+  availability_zone = data.aws_availability_zones.available.names[2]
+
+  tags = {
+	Name = "Private Subnet AZ 1c"
+  }
+}
