aee848166c495ca252f747749a39e31536ce6257
Rodrigo Monteiro <rodrigo@caprica.com.br>
2022-07-07 15:08:50 +0000

    Updated subnets cidr to vars

--- terraform.tfvars
+++ terraform.tfvars
@@ -2,5 +2,8 @@
 vpc_name = "spoke1"
 cidr_block = "10.1.0.0/16"
 
+intra_subnet_1a_cidr = "10.1.0.0/20"
+intra_subnet_1c_cidr = "10.1.16.0/20"
+
 #Transit network destination prefixes
 transit_cidr = "10.0.0.0/16"
--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
 #VPC variables
 variable "vpc_name" {}
 variable "cidr_block" {}
+variable "intra_subnet_1a_cidr" {}
+variable "intra_subnet_1c_cidr" {}
+
 
 #Transit destination CIDR block
 variable "transit_cidr" {}
